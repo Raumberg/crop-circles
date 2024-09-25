3cd530b3a6f224911c0073e2bcf965a756f3adc4
Reisen <96721335+Raumberg@users.noreply.github.com>
2024-09-25 18:07:28 +0300

    Update riegel.py

--- riegel.py
+++ riegel.py
@@ -2,39 +2,157 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch import Tensor
-import math
+from einops import rearrange
 
-class Abstract(nn.Module):
-    def __init__(self, cats: int, dim: int, lr: float = 0.01, use_attention: bool = False, att_dim: int = 0):
-        super(Abstract, self).__init__()
-        self.embedding = nn.Embedding(cats, dim)
-        self.att_dim = att_dim
-        if use_attention:
-            self.attention = AbstractAttention(dim, attention_dim=att_dim)
+from typing import callable
 
-    def forward(self, x: Tensor):
-        x = self.embedding(x)
-        if self.use_attention:
-            x = self.attention
+class Temporality(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
 
-    def extract(self):
-        return self.embedding.weight.detach().numpy()
+    def forward(self, x, **kwargs):
+        return self.fn(x, **kwargs) + x
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class GEGLU(nn.Module):
+    """
+    Gated GELU
+    """
+    def forward(self, x):
+        x, gates = x.chunk(2, dim = -1)
+        return x * F.gelu(gates)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * mult * 2),
+            GEGLU(),
+            nn.Dropout(dropout),
+            nn.Linear(dim * mult, dim)
+        )
+
+    def forward(self, x, **kwargs):
+        return self.net(x)
     
-class AbstractAttention(nn.Module):
-    def __init__(self, input_dim: int, attention_dim: int):
-        super(AbstractAttention, self).__init__()
-        self.query_linear = nn.Linear(input_dim, attention_dim)
-        self.key_linear = nn.Linear(input_dim, attention_dim)
-        self.value_linear = nn.Linear(input_dim, attention_dim)
+class Abberation(nn.Module):
+    def __init__(
+        self,
+        dim,
+        n_streams = 8,
+        dim_stream = 16,
+        dropout = 0.
+    ):
+        super().__init__()
+        inner_dim = dim_stream * n_streams
+        self.n_streams = n_streams
+        self.scale = dim_stream ** -0.5
 
-    def forward(self, x: Tensor):
-        query = self.query_linear(x)
-        key = self.key_linear(x)
-        value = self.value_linear(x)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
 
-        attention_scores = torch.matmul(query, key.T) / math.sqrt(self.attention_dim)
-        attention_weights = F.softmax(attention_scores, dim=1)
+        self.dropout = nn.Dropout(dropout)
 
-        output = attention_weights * value
-        return output.sum(dim=1)
+    def forward(self, x):
+        h = self.n_streams
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'bn(hd)->bhnd', h=h), (q, k, v))
+        sim = torch.einsum('bhid,bhjd->bhij', q, k) * self.scale
+        attn = sim.softmax(dim = -1)
+        out = torch.einsum('bhij,bhjd->bhid', attn, v)
+        out = rearrange(out, 'bhnd->bn(hd)', h = h)
+        return self.to_out(out)
+    
+class Perceptron(nn.Module):
+    def __init__(self, dims):
+        super(Perceptron, self).__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(dims[0], dims[1]),
+            nn.ReLU(),
+            nn.Linear(dims[1], dims[2])
+        )
+        
+    def forward(self, x):
+        if len(x.shape)==1:
+            x = x.view(x.size(0), -1)
+        x = self.layers(x)
+        return x
+
+class Riegel(nn.Module):
+    def __init__(
+            self, 
+            num_tokens, 
+            dim, 
+            n_feats, 
+            depth, 
+            n_streams, 
+            dim_stream, 
+            attn_dropout, 
+            ff_dropout,
+            style='col'
+        ):
+        super().__init__()
+        self.embeds = nn.Embedding(num_tokens, dim)
+        self.layers = nn.ModuleList([])
+        self.mask_embed =  nn.Embedding(n_feats, dim)
+        self.style = style
+        for _ in range(depth):
+            if self.style == 'colrow':
+                self.layers.append(nn.ModuleList([
+                    PreNorm(dim, Temporality(Abberation(
+                        dim, 
+                        n_streams=n_streams, 
+                        dim_stream=dim_stream, 
+                        dropout=attn_dropout))),
+                    PreNorm(dim, Temporality(FeedForward(
+                        dim, 
+                        dropout=ff_dropout))),
+                    PreNorm(dim*n_feats, Temporality(Abberation(
+                        dim*n_feats, 
+                        n_streams=n_streams, 
+                        dim_stream=64, 
+                        dropout=attn_dropout))),
+                    PreNorm(dim*n_feats, Temporality(FeedForward(
+                        dim*n_feats, 
+                        dropout=ff_dropout))),
+                ]))
+            else:
+                self.layers.append(nn.ModuleList([
+                    PreNorm(dim*n_feats, Temporality(Abberation(
+                        dim*n_feats, 
+                        n_streams=n_streams, 
+                        dim_stream=64, 
+                        dropout=attn_dropout))),
+                    PreNorm(dim*n_feats, Temporality(FeedForward(
+                        dim*n_feats, 
+                        dropout = ff_dropout))),
+                ]))
+
+    def forward(self, x, x_cont=None, mask = None):
+        if x_cont is not None:
+            x = torch.cat((x,x_cont),dim=1)
+        _, n, _ = x.shape
+        if self.style == 'colrow':
+            for attn1, ff1, attn2, ff2 in self.layers: 
+                x = attn1(x)
+                x = ff1(x)
+                x = rearrange(x, 'bnd->1b(nd)')
+                x = attn2(x)
+                x = ff2(x)
+                x = rearrange(x, '1b(nb)->bnd', n = n)
+        else:
+             for attn1, ff1 in self.layers:
+                x = rearrange(x, 'bnd->1b(nd)')
+                x = attn1(x)
+                x = ff1(x)
+                x = rearrange(x, '1b(nd)->bnd', n = n)
+        return x
